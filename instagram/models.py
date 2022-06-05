70a82a63f15a86d56ff4e9adbef29de2d5975439
KamauDev-maker <oscarnjenga@gmail.com>
2022-06-05 21:29:39 +0300

    creating the apps models

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    image = models.ImageField(upload_to ='images/')
+    bio = models.TextField(max_length=500,blank=True)
+    name = models.CharField(blank=True)
+    
+    def __str__(self):
+        return f'{self.user.username} profile'
+    
+class Post(models.Model):
+    image = models.ImageField(upload_to ='post/')
+    name = models.CharField(blank=True)
+    likes = models.ManyToManyField(User, related_name='likes',blank=True)
+    user = models.ForeignKey(Profile, on_delete=CASCADE,related_name='post')  
+    created = models.DateTimeField(auto_now_add=True,null=True)
+    
+    def __str__(self):
+        return f'{self.user.name} Post'
+    
+class Comment(models.Model):
+    comment = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='comments')
+    created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return f'{self.user.name} Post'
+    
+    
+class Follow(models.Model):
+    follower = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='following')
+    followed = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='followers')
+
+    def __str__(self):
+        return f'{self.follower} Follow'    
+    
+      
--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+
 
 # Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        return redirect('index')
+    return render (request,'registration/signup.html')
+
+def index (request):
+    if request.method == 'POST':
+        return HttpResponseRedirect(request.path_info)
+    return render(request, 'instagram/index.html')
+
+    
+        
