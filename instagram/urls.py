a1225f32623399d4d046649bf801b417096b2f02
KamauDev-maker <oscarnjenga@gmail.com>
2022-06-05 22:05:26 +0300

    creating the dynamic routes for our views

--- instagram/urls.py
+++ instagram/urls.py
@@ -4,5 +4,13 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = 'index'),
-    path('signup/', views.signup, name ='signup')
+    path('account/', include('django.contrib.auth.urls')),
+    path('signup/', views.signup, name ='signup'),
+    path('profile/<username>/', views.profile, name='profile'),
+    path('user_profile/<username>/', views.user_profile, name='user_profile'),
+    path('post/<id>', views.post_comment, name='comment'),
+    path('like', views.like_post, name='like_post'),
+    path('search/', views.search_profile, name='search'),
+    path('unfollow/<to_unfollow>', views.unfollow, name='unfollow'),
+    path('follow/<to_follow>', views.follow, name='follow')
 ]
