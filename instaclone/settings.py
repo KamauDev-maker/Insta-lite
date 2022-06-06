569f916aebd38c1460c289a3e066d3a4e3db655b
KamauDev-maker <oscarnjenga@gmail.com>
2022-06-06 10:46:33 +0300

    adding post template

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -135,7 +135,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = 'login'
+LOGIN_REDIRECT_URL = 'index'
 
 LOGOUT_REDIRECT_URL = 'login'
 
